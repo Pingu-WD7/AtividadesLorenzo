c8148aa07e2c4ce28e91ff0892412b3bd9184e0e
Pingu <felipe_guaresi@estudante.sesisenai.org.br>
2026-02-27 20:33:45 -0300

    Add files via upload

---
+++ Helpdesk/src/service/Helpdesk.java
@@ -0,0 +1,59 @@
+package service;
+
+import model.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Helpdesk {
+    private ArrayList<Cliente> clientes = new ArrayList<>();
+    private ArrayList<Chamado> chamados = new ArrayList<>();
+
+    //Operações do Cliente
+    public void cadastrarCliente(String nome, String email){
+
+        Cliente c = new Cliente();
+        c.setNome(nome);
+        c.setEmail(email);
+        clientes.add(c);
+    }
+
+    public Cliente buscarCliente(String email){
+        for (Cliente c : clientes){
+            if(c.getEmail().equalsIgnoreCase(email)){
+                return c;
+            }
+        }
+        return null;
+    }
+    public Chamado buscarChamado(int numero){
+        for(Chamado c : chamados){
+            return c;
+        }
+        return null;
+    }
+    public void alterarStatus(int numero, StatusChamado novoStatus){
+        Chamado ch = buscarChamado(numero);
+        if (ch != null){
+            ch.setStatus(novoStatus);
+        }
+    }
+    //Operações do Chamado
+    public void abrirChamado(Integer numero, String titulo, String descricao, PrioridadeChamado prioridade, Cliente cliente){
+        if (numero == null) return;
+
+        //Cliente cli = buscarCliente(email);
+
+        Chamado ch = new Chamado();
+        ch.setNumero(numero);
+        ch.setTitulo(titulo);
+        ch.setData(LocalDate.now());
+        ch.setDescricao(descricao);
+        ch.setPrioridade(prioridade);
+        ch.setStatus(StatusChamado.ABERTO);
+        ch.setCliente(cliente);
+
+        chamados.add(ch);
+
+    }
+}
